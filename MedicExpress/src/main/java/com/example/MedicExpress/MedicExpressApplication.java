94447455125dd20d71f3a7c26cbf327ef95882f8
ahammiche <adelhammiche.pro@gmail.com>
2025-04-07 14:27:56 +0200

    Project initialization

---
+++ MedicExpress/src/main/java/com/example/MedicExpress/MedicExpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedicExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicExpressApplication.class, args);
+	}
+
+}
---
+++ MedicExpress/src/test/java/com/example/MedicExpress/MedicExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedicExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
