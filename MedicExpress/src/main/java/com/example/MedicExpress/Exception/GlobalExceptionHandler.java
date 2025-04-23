42961d1ea16b7048cf0743b774178d958cf2d244
ahammiche <adelhammiche.pro@gmail.com>
2025-04-23 15:23:23 +0200

    global exception handler -> permit to get the exceptions thrown and act

---
+++ MedicExpress/src/main/java/com/example/MedicExpress/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.MedicExpress.Exception;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserDoesNotExistException.class)
+    public ResponseEntity<String> handleRuntimeException(UserDoesNotExistException ex) {
+        return ResponseEntity.status(404).body("Global handler: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<String> handleRuntimeException(UserAlreadyExistException ex) {
+        return ResponseEntity.status(500).body("Global handler: " + ex.getMessage());
+    }
+}
