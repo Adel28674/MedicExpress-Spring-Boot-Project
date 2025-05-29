8680baf0eeb2cd646b0e235fb663e3a10fedd3cd
Yanis BOUCHAIB <yanisbouchaib28@gmail.com>
2025-05-29 15:18:26 +0200

    search delivery to creat order

--- MedicExpress/src/main/java/com/example/MedicExpress/Service/PatientService.java
+++ MedicExpress/src/main/java/com/example/MedicExpress/Service/PatientService.java
@@ -34,35 +34,5 @@ public class PatientService {
         return orderRepository.findByPatientId(patientId);
     }
 
-    public OrderEntity createOrder(CreateOrderRequest request) {
-        DeliveryDriverEntity deliveryDriver = deliveryDriverRepository.findById(request.getDeliveryDriverId())
-                .orElseThrow(() -> new RuntimeException("Delivery Driver not found"));
 
-        PharmacyEntity pharmacy = pharmacyRepository.findById(request.getPharmacyId())
-                .orElseThrow(() -> new RuntimeException("Pharmacy not found"));
-
-        PatientEntity patient = patientRepository.findById(request.getPatientId())
-                .orElseThrow(() -> new RuntimeException("Patient not found"));
-
-        PrescriptionEntity prescription = prescriptionRepository.findById(request.getPrescriptionId())
-                .orElseThrow(() -> new RuntimeException("Prescription not found"));
-
-        OrderEntity order = new OrderEntity();
-        order.setDate(new java.sql.Date(System.currentTimeMillis()));
-        order.setStatus("Pending");
-        order.setDeliveryDriver(deliveryDriver);
-        order.setPharmacy(pharmacy);
-        order.setPatient(patient);
-        order.setPrescription(prescription);
-
-        try {
-            String qrText = "Commande ID: " + System.currentTimeMillis(); // ou tout autre identifiant unique
-            String qrCodeBase64 = QRCodeGenerator.generateQRCodeBase64(qrText, 200, 200);
-            order.setQrcode(qrCodeBase64);
-        } catch (WriterException | IOException e) {
-            throw new RuntimeException("Erreur lors de la génération du QR code", e);
-        }
-
-        return orderRepository.save(order);
-    }
 }
